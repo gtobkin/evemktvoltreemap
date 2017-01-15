4efdacaf6574ec52c62848dd8a025ec7c488ac57
Greg Tobkin <greg.tobkin@gmail.com>
2017-01-15 14:25:21 -0800

    Print out trit market history

--- crest_test.py
+++ crest_test.py
@@ -1,5 +1,12 @@
 import requests, pprint
+# print out all endpoints
 url = "https://crest-tq.eveonline.com"
+json = requests.get(url).json()
+pp = pprint.PrettyPrinter(indent=4, depth=6)
+pp.pprint(json)
+
+# print out trit market history specifically
+url = "https://crest-tq.eveonline.com/market/10000002/history/?type=https://crest-tq.eveonline.com/inventory/types/34/"
 json = requests.get(url).json()
 pp = pprint.PrettyPrinter(indent=4, depth=6)
 pp.pprint(json)
