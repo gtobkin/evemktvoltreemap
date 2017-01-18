8a3e360da5eeafc3904d26d52c47006b1fef44da
Greg Tobkin <greg.tobkin@gmail.com>
2017-01-17 21:39:39 -0800

    Construct market tree

--- src/gen_daily_mkt_vol_json.py
+++ src/gen_daily_mkt_vol_json.py
@@ -1,12 +1,60 @@
 # imports
+import requests, pprint
+
+# constants
+CREST_URL_ENDPOINTS	= "https://crest-tq.eveonline.com"
+CREST_URL_MARKETTYPES	= "https://crest-tq.eveonline.com/market/types/"
+CREST_URL_MARKETGROUPS	= "https://crest-tq.eveonline.com/market/groups/"
+
+FILE_MARKETTYPES	= "../txt/markettypes.txt"
+FILE_MARKETGROUPS	= "../txt/marketgroups.txt"
+
+# tree code
+class MarketNode(object):
+	def __init__(self):
+		self.id			= None	# could be a market group id or a market type id
+		self.crest_type_url	= None
+		self.parent		= None
 
 # pull API calls
+endpoints = requests.get(CREST_URL_ENDPOINTS).json()
 
 # verify our necessary API calls are supported
+# just kidding - we can't, CCP doesn't list all of them
+# ideally we'd verify that 'endpoints' contains the needed services
+# and use the provided URLs from that instead of (possibly obsolete) constants
 
-# pull market groups and store in .json file
+# pull market types and groups, store dicts in text files for human reference
+marketTypesData = requests.get(CREST_URL_MARKETTYPES).json()
+with open(FILE_MARKETTYPES, 'wt') as outfile:
+	pprint.pprint(marketTypesData, stream=outfile)
 
-# given market groups JSON file, construct market tree
+marketGroupsData = requests.get(CREST_URL_MARKETGROUPS).json()
+with open(FILE_MARKETGROUPS, 'wt') as outfile:
+	pprint.pprint(marketGroupsData, stream=outfile)
+
+# given market types and groups JSON files, construct market tree
+# first create all nodes, then create all parent links (once the parent is guaranteed to exist)
+marketTypes = {}
+for marketType in marketTypesData["items"]:
+	node			= MarketNode()
+	node.id			= marketType["id"]
+	node.crest_type_url 	= marketType["type"]["href"]
+	marketTypes[marketType["id"]] = node
+marketGroups = {}
+for marketGroup in marketGroupsData["items"]:
+	node			= MarketNode()
+	node.id			= marketGroup["id"]
+	node.crest_type_url	= marketGroup["href"]
+	marketGroups[marketGroup["id"]] = node
+for marketType in marketTypesData["items"]:
+	marketTypes[marketType["id"]].parent = marketGroups[marketType["marketGroup"]["id"]]
+for marketGroup in marketGroupsData["items"]:
+	if "parentGroup" in marketGroup: # not all market groups have parents
+		marketGroups[marketGroup["id"]].parent = marketGroups[marketGroup["parentGroup"]["id"]]
+
+# print len(marketTypes) 1000
+# print len(marketGroups) 2157
 
 # copy market tree; we now have trees for yesterday + today
 
